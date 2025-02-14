cc8052ee30de80d7687c83fefe8ae903847a3d58
SandySaadeh <s.saadeh@students.uu.nl>
2025-02-14 16:35:48 +0100

    so far

--- Week2-SandySaadeh/excersize3.R
+++ Week2-SandySaadeh/excersize3.R
@@ -14,6 +14,32 @@ rows_all_coloumns <- data[c(1:50),]
 rows_all_coloumns
 rows_coloumns <- data[c(11:101),c(1:7)]
 rows_coloumns
-#9
-data_depth <- data[depth> 1200]
+head(data)
+data_depth <- data[data$depth >1200, ]
 data_depth
+data_gradient <- data[data$gradient>200, ]
+data_gradient
+data_median <- median(data$gradient)
+data_median
+data_gradient_median <- data['month'=="October" & 'water.noise'=="low" & 'gradient'>data_median, ]
+data_gradient_median
+mean_whales <- mean(data$number.whales, na.rm = TRUE)
+var1 <- data[data$depth > 1500 & data$number.whales > mean_whales,]
+var1
+# Use the subset() function to extract all rows in ’May’ with a time at station
+#less than 1000 minutes and a depth greater than 1000 m. Also use subset() 
+#to extract data collected in ‘October’ from latitudes greater than 61 degrees
+#but only include the columns month, latitude, longitude and number.whales.
+var2 <- subset(data,month=="May" &
+                 time.at.station<1000 & depth>1000 )
+var2
+whale.depth.sort <- data[order(data$depth),]
+whale.depth.sort
+using_aggregate <- aggregate(data[,c(2,4,7,8)], by= list(month= data$month), FUN = mean, na.rm= TRUE)
+using_aggregate
+#for each level for water noise
+table <- table(data$water.noise)
+table
+the_file <- "C:\\Users\\sandy\\Downloads\\whaledata.txt"
+write.table(whale.num, file= the_file, 
+                                col.names = TRUE, row.names = FALSE, sep = "\t")
